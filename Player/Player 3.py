75a219d7c9425e855e01d037dffc76c96980d3f8
Koteyka <romacat5000@gmail.com>
2021-10-24 18:25:48 +0300

    ...

--- Player/Player 3.py
+++ Player/Player 3.py
@@ -7,16 +7,20 @@ canvas = Canvas(root, width=400, height=400)
 canvas.pack()
 
 
-def check_wall(where, hammer):
-    if hammer:
+def check_wall(where, self):
+    if self.hammer:
         return 1
+    print(self.walls)
+    xy = self.get_coords()  # (x1, y1, x2, y2) PLayer
 
     if where == 'w':
         pass
     elif where == 'a':
         pass
     elif where == 's':
-        pass
+        for q, w in self.walls:
+            pass
+
     elif where == 'd':
         pass
 
@@ -25,6 +29,7 @@ def check_wall(where, hammer):
 
 class Hero:
     def __init__(self):
+        self.walls = []
         self.obj = canvas.create_oval(10, 10, 25, 25, fill='red')
         self.x_right = 10
         self.x_left = -10
@@ -41,22 +46,23 @@ class Hero:
             x = randint(0, 400)
             y = randint(0, 400)
             canvas.create_oval(x, y, x + 15, y + 15, fill='blue')
-            walls.append((x, y, x + 15, y + 15))
+            walls.append((x, y))  # x + 15, y + 15
+        self.walls = walls
 
     def move_right(self, event):
-        if check_wall('d', self.hammer):
+        if check_wall('d', self):
             canvas.move(self.obj, self.x_right, 0)
 
     def move_left(self, event):
-        if check_wall('a', self.hammer):
+        if check_wall('a', self):
             canvas.move(self.obj, self.x_left, 0)
 
     def move_up(self, event):
-        if check_wall('w', self.hammer):
+        if check_wall('w', self):
             canvas.move(self.obj, 0, self.y_up)
 
     def move_down(self, event):
-        if check_wall('s', self.hammer):
+        if check_wall('s', self):
             canvas.move(self.obj, 0, self.y_down)
 
     def get_coords(self):
